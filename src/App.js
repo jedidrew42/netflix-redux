5126129bac43e20c67147b53429083d457f23891
Drew Norman <drewnorman1226@gmail.com>
2021-02-22 17:11:30 -0500

    hooking up firebase auth

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import HomeScreen from './screens/HomeScreen';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -6,6 +6,11 @@ import LoginScreen from "./screens/LoginScreen";
 
 function App() {
   const user = null;
+
+  useEffect(() => {
+    auth
+
+  }, [])
 
   return (
     <div className="app">
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
     apiKey: "AIzaSyCvyTr8QULFpX2yp5LKa7kVMSVGMNMNHt4",
     authDomain: "netflix-clone-15190.firebaseapp.com",
@@ -7,4 +9,11 @@ const firebaseConfig = {
     appId: "1:1018369405557:web:e1821ce19dc990f41c3e3c",
     measurementId: "G-4V9KWRX6KN"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebase.auth();
+
+  export { auth };
+  export default db;
 
